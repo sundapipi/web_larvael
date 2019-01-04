aafa3640ed8abf3e691080c3cd930bc0d3738191
SunShaoLei <76840470@qq.com>
2019-01-04 08:58:17 +0800

    添加测是版本撤销

--- app/Http/Controllers/Home/IndexController.php
+++ app/Http/Controllers/Home/IndexController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers\Home;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
 
 class IndexController extends Controller
 {
     //
     function index(Request $request){
-        return 11421211;
+//        return 11421211;
+    
+        DB::table('table_user')->where('连贯操作','where 条件')->get();
     }
 }
